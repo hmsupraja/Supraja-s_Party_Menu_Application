993b76139630812ded41f3b9b21f10da0af85991
SUPRAJA HASTHALAMANGALI <hmsupraja@gmail.com>
2026-07-19 11:58:14 +0530

    Implement API authentication and save recipe functionality

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,17 +1,52 @@
 
 import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import recipes from "../data/recipes";
 
 function RecipeDetails() {
   const { id } = useParams();
-
+  const [saved, setSaved] = useState(false);
   const recipe = recipes.find(
     (item) => item.id === Number(id)
   );
+  useEffect(() => {
+  const savedRecipes =
+    JSON.parse(
+      localStorage.getItem("party_menu_saved_recipes")
+    ) || [];
+
+  const exists = savedRecipes.some(
+    (item) => item.id === recipe?.id
+  );
+
+  setSaved(exists);
+}, [recipe]);
 
   if (!recipe) {
     return <h2>Recipe not found</h2>;
   }
+
+  const handleSaveRecipe = () => {
+  let savedRecipes =
+    JSON.parse(
+      localStorage.getItem("party_menu_saved_recipes")
+    ) || [];
+
+  if (saved) {
+    savedRecipes = savedRecipes.filter(
+      (item) => item.id !== recipe.id
+    );
+  } else {
+    savedRecipes.push(recipe);
+  }
+
+  localStorage.setItem(
+    "party_menu_saved_recipes",
+    JSON.stringify(savedRecipes)
+  );
+
+  setSaved(!saved);
+};
 
   return (
     <div>
@@ -36,7 +71,14 @@ function RecipeDetails() {
       </ul>
 
       <h4>{recipe.servings}</h4>
-      <Link to="/">
+      <button onClick={handleSaveRecipe}>
+  {saved ? "Saved ❤️" : "Save Recipe"}
+</button>
+
+<br />
+<br />
+
+<Link to="/">
   <button>← Back to Home</button>
 </Link>
     </div>
